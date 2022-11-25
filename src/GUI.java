f0e8a0c24546fea329f5ed481d239d0a801078e5
bean-b <bbaumsta@macalester.edu>
2022-11-24 22:10:59 -0600

    gui

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,40 @@
 package src;
 
-public class GUI {
+
+import javax.swing.*;
+
+public class GUI{
+    private static JFrame frame;
+    private static JTextArea textField;
+    private static JTextArea outPutField;
+    private static JTextArea theStackField;
     public static void main(String[] args) {
-        
+        constructGUI();
+    }
+
+    private static void constructGUI(){
+        frame = new JFrame("Stackey");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920,1080);
+ 
+        JPanel panel = new JPanel();
+ 
+ 
+        textField = new JTextArea(25, 25);
+        outPutField = new JTextArea(25, 25);
+        outPutField.setEditable(false);
+        theStackField = new JTextArea(25, 25);
+        theStackField.setEditable(false);
+        JButton runAllButton = new JButton("Run All");
+        JButton incrementButton = new JButton("Increment Line");
+        panel.add(textField);
+        panel.add(runAllButton);
+        panel.add(incrementButton);
+        panel.add(outPutField);
+        panel.add(theStackField);
+ 
+ 
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
     }
 }
